f9edcbaeba86a4f854485e1779668cc1649bfcf6
eirikur1 <eirikurak@gmail.com>
2023-11-02 23:15:23 +0000

    first commit

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-import { createCartLine, showCartContent } from './lib/ui.js';
+import { createCartLine, showCartContent } from "./lib/ui.js";
 
 const products = [
   {
     id: 1,
-    title: 'HTML húfa',
+    title: "HTML húfa",
     description:
-      'Húfa sem heldur hausnum heitum og hvíslar hugsanlega að þér hvaða element væri best að nota.',
+      "Húfa sem heldur hausnum heitum og hvíslar hugsanlega að þér hvaða element væri best að nota.",
     price: 5_000,
   },
   {
     id: 2,
-    title: 'CSS sokkar',
-    description: 'Sokkar sem skalast vel með hvaða fótum sem er.',
+    title: "CSS sokkar",
+    description: "Sokkar sem skalast vel með hvaða fótum sem er.",
     price: 3_000,
   },
   {
     id: 3,
-    title: 'JavaScript jakki',
-    description: 'Mjög töff jakki fyrir öll sem skrifa JavaScript reglulega.',
+    title: "JavaScript jakki",
+    description: "Mjög töff jakki fyrir öll sem skrifa JavaScript reglulega.",
     price: 20_000,
   },
 ];
@@ -25,13 +25,13 @@ const products = [
 /** Bæta vöru í körfu */
 function addProductToCart(product, quantity) {
   // Hér þarf að finna `<tbody>` í töflu og setja `cartLine` inn í það
-  const cart = document.querySelector('.cart-content');
+  const cart = document.querySelector(".cart-content");
 
   if (!cart) {
-    console.warn('fann ekki .cart');
+    console.warn("fann ekki .cart");
     return;
   }
-  
+
   // TODO hér þarf að athuga hvort lína fyrir vöruna sé þegar til
   const cartLine = createCartLine(product, quantity);
   cart.appendChild(cartLine);
@@ -45,9 +45,9 @@ function addProductToCart(product, quantity) {
 function submitHandler(event) {
   // Komum í veg fyrir að form submiti
   event.preventDefault();
-  
+
   // Finnum næsta element sem er `<tr>`
-  const parent = event.target.closest('tr')
+  const parent = event.target.closest("tr");
 
   // Það er með attribute sem tiltekur auðkenni vöru, t.d. `data-product-id="1"`
   const productId = Number.parseInt(parent.dataset.productId);
@@ -64,12 +64,35 @@ function submitHandler(event) {
 }
 
 // Finna öll form með class="add"
-const addToCartForms = document.querySelectorAll('.add')
 
 // Ítra í gegnum þau sem fylki (`querySelectorAll` skilar NodeList)
 for (const form of Array.from(addToCartForms)) {
   // Bæta submit event listener við hvert
-  form.addEventListener('submit', submitHandler);
+  form.addEventListener("submit", submitHandler);
 }
 
 // TODO bæta við event handler á form sem submittar pöntun
+
+/* const addToCartForms = document.querySelectorAll.('.add')
+
+ function submitHandler(event) {
+event.preventDefault();
+
+const parnet = event.target.closest('tr´')
+    console.log(parent.dataset)
+    const priductId=parent.dataset.productId
+debugger
+  const product = products.find((i)) => i.id === productId);
+  
+  console.log(product)
+}
+
+function createAddToCartForm(form) {
+ //console.log(form)
+form.addEventListener('sumbit', submitHandler)
+}
+
+for (const form of array.from(addToCartForms)) {
+ createAddToCartForm(form);
+}
+*/ 
